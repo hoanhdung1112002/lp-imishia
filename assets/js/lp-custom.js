203aa255a7955308fbab0d09e4ce4523019d6448
hadung2002 <79574082+hoanhdung1112002@users.noreply.github.com>
2024-09-13 16:53:32 +0700

    update 1392024

--- assets/js/lp-custom.js
+++ assets/js/lp-custom.js
@@ -25,18 +25,32 @@ jQuery(function($){
     });
 
     //faq
-    $(".faq-item").click(function() {
-        $(".faq-inner-content").slideUp();
-    
-        if ($(this).find(".faq-inner-content").is(":visible")) {
-            $(this).find(".faq-y").css("transform", "rotate(-90deg)")
-            $(this).find(".faq-inner-content").slideUp();
-
+    $('.faq-title').on("click", function () {
+        var $content = $(this).closest(".faq-item").find(".faq-content");
+        var $faqplus = $(this).closest(".faq-item").find(".faq-plus");
+        $(".faq-plus").removeClass("anime-add");
+        $(".faq-plus").removeClass("anime-remove");
+        $(".item-content").slideUp("200");
+        if ($content.is(":hidden")) {
+        $faqplus.addClass("anime-add");
+        $content.slideDown("200");
         } else {
-
-            $(this).find(".faq-inner-content").slideDown();
-            $(this).find(".faq-y").css("transform", "rotate(0deg)")
+        $content.slideUp("200");
+        $faqplus.removeClass("anime-add");
+        $faqplus.addClass("anime-remove");
         }
     });
-    
+
+    $('.faq-item-title').on("click", function () {
+        var $content = $(this).closest("li").find(".faq-item-content");
+        $(".faq-item-content").slideUp("200");
+        if ($content.is(":hidden")) {
+        $("li").removeClass("active");
+        $content.slideDown("200");
+        $(this).closest("li").addClass("active");
+        } else {
+        $content.slideUp("200");
+        $(this).closest("li").removeClass("active");
+        }
+    });
 });
